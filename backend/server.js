29b6ff971e9f020f55b6c2e1ca6375bd47325328
MrJCrawford <azwin.lam@gmail.com>
2020-12-15 10:56:34 +0800

    added api and removed 5000

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ connection.once('open', () => {
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+app.use('/api/exercises', exercisesRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
