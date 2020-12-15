ba58f08fa350af45d0704cb5585ce498f08c4bbc
MrJCrawford <azwin.lam@gmail.com>
2020-12-15 13:47:08 +0800

    run node backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000
+
 app.use(cors());
 app.use(express.json());
 
@@ -25,6 +26,9 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
+
+
+const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
